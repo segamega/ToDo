e774571e98a4936d24b680c24b702386b342dc14
Журавлев Сергей <sergei.zhuravlev@simbirsoft.com>
2023-10-22 12:18:15 +0300

    add create task page

---
+++ src/Web.App/Models/Dto/TaskDto.cs
@@ -0,0 +1,40 @@
+﻿using Web.App.Models.Enum;
+
+namespace Web.App.Models.Dto
+{
+    /// <summary>
+    /// Задача.
+    /// </summary>
+    public class TaskDto
+    {
+        /// <summary>
+        /// Идентификатор.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Идентификатор проекта.
+        /// </summary>
+        public int ProjectName { get; set; }
+
+        /// <summary>
+        /// Заголовок.
+        /// </summary>
+        public string Title { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Описание.
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Срок выполнения.
+        /// </summary>
+        public DateTime? DueDate { get; set; }
+
+        /// <summary>
+        /// Приоритет.
+        /// </summary>
+        public PriorityType PriorityType { get; set; }
+    }
+}
